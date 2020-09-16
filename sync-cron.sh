c0d8fef793271fcdb3ad005ae8c6db4031bdf3f2
James Antill <james@and.org>
2020-09-16 12:33:18 -0400

    Add locking so we can run a lot and not go twice at once.

--- sync-cron.sh
+++ sync-cron.sh
@@ -20,6 +20,23 @@ nocache=--nocache
 nocache=
 # ----------------
 
+lockf=sync2git-lock-file.lock
+function unlockf {
+    rm -f "$lockf"
+}
+
+tm=60
+while [ -f "$lockf" ]; do
+  echo "Now: $(date --iso=minutes)"
+  echo "File <$lockf> already EXISTS! Owned by pid: $(cat $lockf)"
+  echo " C-c in next $tm seconds or WAIT."
+  sleep $tm
+done
+
+echo "$$" > "$lockf"
+trap unlockf EXIT
+
+
 # Find the dir. this script is in...
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR
