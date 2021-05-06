efb9e4b741b222615846a447b053c53f884c6036
James Antill <james@and.org>
2021-05-05 23:02:10 -0400

    Add timeout comments/etc.

--- cron-sync2build.sh
+++ cron-sync2build.sh
@@ -9,10 +9,14 @@ function unlockf {
     rm -f "$lockf"
 }
 
+# ./mtimecache.py secs 64m
+# secs: 3840
+# It 2x upto this, so it doesn't sleep(64m) but the total sleep time is ~64m.
+tmout=3840
 tm=15
 while [ -f "$lockf" ]; do
   echo "Now: $(date --iso=minutes)"
-  if [ $tm -ge 1920 ]; then
+  if [ $tm -ge $tmout ]; then
     echo "Exiting."
     exit 1
   fi
